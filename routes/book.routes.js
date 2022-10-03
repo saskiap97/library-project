b77a2b7e0079c4126d2183ed9d6551c16e124dd8
Saskia Peach <saskiapeach@gmail.com>
2022-10-03 15:18:53 +0100

    add book routes, create book, redirect

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -14,4 +14,48 @@ router.get("/books", (req, res, next) => {
   .catch(err=> console.log('there was an error', err))
 });
 
+
+//Book details
+router.get("/books/:bookId", (req, res, next) => {
+    const id = req.params.bookId;
+  
+    Book.findById(id)
+      .then( bookDetails => {
+        res.render("books/book-details", bookDetails)
+      } )
+      .catch( err => {
+        console.log("error getting book details from DB", err);
+        next();
+      })
+  });
+
+//Create a route to display a form
+
+router.get("/books/create", (req, res, next)=> {
+res.render('books/book-create')
+})
+
+//Create a route to process form info 
+
+router.post("/books/create", (req, res, next) => {
+  
+    const bookDetails = {
+      title: req.body.title,
+      description: req.body.description,
+      author: req.body.author,
+      rating: req.body.rating,
+    }
+  
+    Book.create(bookDetails)
+      .then(bookDetails => {
+        res.redirect("/books")
+      })
+      .catch(err => {
+        console.log("error creating new book in DB", err);
+        next();
+      })
+  
+  })
+  
+
 module.exports = router;
